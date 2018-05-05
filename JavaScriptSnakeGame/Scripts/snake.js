5948d4b483d51a4c9f7ed82a45600cdf86d45c4b
ndomitio <nicholas.domitio@gmail.com>
2018-05-05 19:09:57 -0400

    keystrokesadded

--- JavaScriptSnakeGame/Scripts/snake.js
+++ JavaScriptSnakeGame/Scripts/snake.js
@@ -11,8 +11,8 @@ var tailX = [snakeX];
 var tailY = [snakeY];
 var fX;
 var fY;
-var running;
-var gameOver;
+var running = false;
+var gameOver = false;
 var direction = -1; //up = 0, down = -1 left =1, right = 2;
 var int;
 
@@ -56,13 +56,13 @@ function createMap() {
 
 function createSnake() {
     set(snakeX, snakeY, "snake");
-    
+
 }
 function get(x, y) {
-    return document.getElementById(x+"-"+y);
+    return document.getElementById(x + "-" + y);
 }
 function set(x, y, value) {
-    get(x,y).setAttribute("class", value);
+    get(x, y).setAttribute("class", value);
 }
 
 function rand(min, max) {
@@ -84,5 +84,46 @@ function createFruit() {
     fX = fruitX;
     fY = fruitY;
 }
+
+window.addEventListener("keypress", function key() {
+    //if key is W set direction up
+    var key = event.keyCode;
+    if (direction != -1 && (key == 199 || key == 87))
+        direction = 0;
+    //if key is S set direction down;
+    else if (direction != 0 && (key == 115 || key == 83))
+        direction = -1;
+    //if key is A set direction left;
+    else if (direction != 2 && (key == 97 || key == 65))
+        direction = 1;
+    //if key is D set direction right;
+    else if (direction != 1 && (key == 100 || key == 68))
+        direction = 2;
+    if (!running)
+        running == true;
+    else if (key == 32)
+        running = false;
+});
+
+function gameLoop() {
+    if (running && !gameOver) {
+        update();
+    } else if (gameOver) {
+        clearInterval(int);
+    }
+}
+function update() {
+    setBlock(fx, fy, "fruit");
+    set(tailX[length], tailY[length], "blank");
+    if (direction == 0)
+        snakeY--;
+    else if (direction == -1)
+        snakeY++;
+    else if (direction == 1)
+        snakeX--;
+    else if (direction == 2)
+        snakeX--;
+}
+
 
 run();
